61b4da2d11be25134c713269358f343b19f9a1be
stognole <stognole@b-tu.de>
2017-05-30 14:59:23 +0200

    Files created

---
+++ src/m/Actor.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Ole on 30.05.2017.
+ */
+"use strict";
+
+/**
+ * Class Actor ##############################################################
+ */
+
+class Actor extends Person {
+
+}
---
+++ src/v/persons/listPersons.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Ole on 30.05.2017.
+ */
+
+"use strict";
+
+pl.v.retrieveAndListPersons = {
+    setupUserInterface: function () {
+        let table = document.getElementById("personOutput");
+        let keys, key, i, row;
+
+        pl.c.app.retrieveAllData();
+        keys = Object.keys(Person.instances);
+
+        for (i = 0; i < keys.length; i += 1) {
+            key = keys[i];
+
+            row = table.insertRow(-1); // -1 adds row at the end of the table
+            row.insertCell(-1).innerHTML = Person.instances[key].personId + "";
+            row.insertCell(-1).innerHTML = Person.instances[key].name;
+        }
+    }
+};
